add29b0eaa3ffd1b9f1724c159a84b9c797e927e
jinoc <choiae369@gmail.com>
2023-06-09 12:46:21 +0900

    슬라이드 오류 수정중2

--- static/modules/moveslide.js
+++ static/modules/moveslide.js
@@ -5,25 +5,91 @@ export function moveSlide(movies) {
   let sliderBtndoc = document.querySelector('.slider-btn');
   const prev = document.querySelector('.prev');
   const next = document.querySelector('.next');
-  
+
+  let clickCount = 0;
+  let sW = slides.offsetWidth;
+  let posX = (clickCount * sW) / slideCount;
+
   if (slideCount <= 5) {
     sliderBtndoc.style.display = 'none';
   } else {
     sliderBtndoc.style.display = 'flex';
   }
-  
+
   if (screenWidth < 768) {
     slides.style.width = 50 * slideCount + '%';
+    sW = slides.offsetWidth;
+    posX = (clickCount * sW) / slideCount;
+    prev.addEventListener('click', function () {
+      if (clickCount === 0) {
+        return false;
+      } else {
+        clickCount--;
+        posX = (clickCount * sW) / slideCount;
+        slides.style.transform = `translateX(-${posX}px)`;
+      }
+    });
+  
+    next.addEventListener('click', function () {
+      if (clickCount < slideCount - 4) {
+        clickCount++;
+        posX = (clickCount * sW) / slideCount;
+        slides.style.transform = `translateX(-${posX}px)`;
+      } else {
+        return false;
+      }
+    });
   } else if (screenWidth < 1200) {
     slides.style.width = 33.333 * slideCount + '%';
+    sW = slides.offsetWidth;
+    posX = (clickCount * sW) / slideCount;
+    prev.addEventListener('click', function () {
+      if (clickCount === 0) {
+        return false;
+      } else {
+        clickCount--;
+        posX = (clickCount * sW) / slideCount;
+        slides.style.transform = `translateX(-${posX}px)`;
+      }
+    });
+  
+    next.addEventListener('click', function () {
+      if (clickCount < slideCount - 3) {
+        clickCount++;
+        posX = (clickCount * sW) / slideCount;
+        slides.style.transform = `translateX(-${posX}px)`;
+      } else {
+        return false;
+      }
+    });
   } else {
     slides.style.width = 20 * slideCount + '%';
+    sW = slides.offsetWidth;
+    posX = (clickCount * sW) / slideCount;
+    prev.addEventListener('click', function () {
+      if (clickCount === 0) {
+        return false;
+      } else {
+        clickCount--;
+        posX = (clickCount * sW) / slideCount;
+        slides.style.transform = `translateX(-${posX}px)`;
+      }
+    });
+  
+    next.addEventListener('click', function () {
+      if (clickCount < slideCount - 5) {
+        clickCount++;
+        posX = (clickCount * sW) / slideCount;
+        slides.style.transform = `translateX(-${posX}px)`;
+      } else {
+        return false;
+      }
+    });
   }
 
   slides.style.minWidth = '100%';
-  let clickCount = 0;
-  let sW = slides.offsetWidth;
-  let posX = (clickCount * sW) / slideCount;
+  // sW = slides.offsetWidth;
+  // posX = (clickCount * sW) / slideCount;
 
   window.onresize = function (event) {
     let screenWidth = window.innerWidth;
@@ -32,94 +98,36 @@ export function moveSlide(movies) {
       sW = slides.offsetWidth;
       posX = (clickCount * sW) / slideCount;
       slides.style.transform = `translateX(-${posX}px)`;
-      prev.addEventListener('click', function () {
-        if (clickCount === 0) {
-          return false;
-        } else {
-          clickCount--;
-          posX = (clickCount * sW) / slideCount;
-          slides.style.transform = `translateX(-${posX}px)`;
-        }
-      });
-    
-      next.addEventListener('click', function () {
-        if (clickCount < slideCount - 2) {
-          clickCount++;
-          posX = (clickCount * sW) / slideCount;
-          slides.style.transform = `translateX(-${posX}px)`;
-        } else {
-          return false;
-        }
-      });
     } else if (screenWidth < 1200) {
       slides.style.width = 33.333 * slideCount + '%';
       sW = slides.offsetWidth;
       posX = (clickCount * sW) / slideCount;
       slides.style.transform = `translateX(-${posX}px)`;
-      prev.addEventListener('click', function () {
-        if (clickCount === 0) {
-          return false;
-        } else {
-          clickCount--;
-          posX = (clickCount * sW) / slideCount;
-          slides.style.transform = `translateX(-${posX}px)`;
-        }
-      });
-    
-      next.addEventListener('click', function () {
-        if (clickCount < slideCount - 2) {
-          clickCount++;
-          posX = (clickCount * sW) / slideCount;
-          slides.style.transform = `translateX(-${posX}px)`;
-        } else {
-          return false;
-        }
-      });
     } else {
       slides.style.width = 20 * slideCount + '%';
       sW = slides.offsetWidth;
       posX = (clickCount * sW) / slideCount;
       slides.style.transform = `translateX(-${posX}px)`;
-      prev.addEventListener('click', function () {
-        if (clickCount === 0) {
-          return false;
-        } else {
-          clickCount--;
-          posX = (clickCount * sW) / slideCount;
-          slides.style.transform = `translateX(-${posX}px)`;
-        }
-      });
-    
-      next.addEventListener('click', function () {
-        if (clickCount < slideCount - 5) {
-          clickCount++;
-          posX = (clickCount * sW) / slideCount;
-          slides.style.transform = `translateX(-${posX}px)`;
-        } else {
-          return false;
-        }
-      });
     }
   };
 
-  prev.addEventListener('click', function () {
-    if (clickCount === 0) {
-      return false;
-    } else {
-      clickCount--;
-      posX = (clickCount * sW) / slideCount;
-      slides.style.transform = `translateX(-${posX}px)`;
-    }
-  });
+  // prev.addEventListener('click', function () {
+  //   if (clickCount === 0) {
+  //     return false;
+  //   } else {
+  //     clickCount--;
+  //     posX = (clickCount * sW) / slideCount;
+  //     slides.style.transform = `translateX(-${posX}px)`;
+  //   }
+  // });
 
-  next.addEventListener('click', function () {
-    if (clickCount < slideCount - 5) {
-      clickCount++;
-      posX = (clickCount * sW) / slideCount;
-      slides.style.transform = `translateX(-${posX}px)`;
-    } else {
-      return false;
-    }
-  });
-
+  // next.addEventListener('click', function () {
+  //   if (clickCount < slideCount - 5) {
+  //     clickCount++;
+  //     posX = (clickCount * sW) / slideCount;
+  //     slides.style.transform = `translateX(-${posX}px)`;
+  //   } else {
+  //     return false;
+  //   }
+  // });
 }
