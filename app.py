486cadcab01c17c231ff530efe4412d38249a66b
JellyBear97 <olxloha@gamil.com>
2023-06-09 12:19:57 +0900

    [수정] 모바일 title css 수정

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ def get_reviews():
     return jsonify({"result": result})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
